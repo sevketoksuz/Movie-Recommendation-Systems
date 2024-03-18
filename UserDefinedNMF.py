468fe95bda0575bfcea323bba0764dfece6f17e9
sevketoksuz <98739815+sevketoksuz@users.noreply.github.com>
2024-03-18 10:56:17 +0300

    Add files via upload

--- UserDefinedNMF.py
+++ UserDefinedNMF.py
@@ -2,20 +2,23 @@ import numpy as np
 
 class UserDefinedNMF:
     
-    def __init__(self, n_factors=15, n_epochs=30, reg_pu=0.12, reg_qi=0.12, reg_bu=0.04, reg_bi=0.02, lr_bu=0.01, lr_bi=0.01, learning_rate=0.01, user_id_map=None, item_id_map=None):
+    def __init__(self, n_factors=15, n_epochs=30, 
+                reg_pu=0.12, reg_qi=0.12, learning_rate=0.01,
+                #,reg_bu=0.04, reg_bi=0.02, lr_bu=0.01, lr_bi=0.01 
+                user_id_map=None, item_id_map=None):
         self.n_factors = n_factors
-        self.n_epochs = n_epochs
-        self.reg_pu = reg_pu
-        self.reg_qi = reg_qi
-        self.reg_bu = reg_bu
-        self.reg_bi = reg_bi
-        self.lr_bu = lr_bu
-        self.lr_bi = lr_bi
+        self.n_epochs = n_epochs 
+        self.reg_pu = reg_pu #used
+        self.reg_qi = reg_qi #used
         self.learning_rate = 0.01
         self.user_id_map = user_id_map
         self.item_id_map = item_id_map
 
-
+        #self.reg_bu = reg_bu
+        #self.reg_bi = reg_bi 
+        #self.lr_bu = lr_bu
+        #self.lr_bi = lr_bi
+    
     def init_factors(self, num_factors, size):
         return np.abs(np.random.normal(0, .1, (size, num_factors)))
     
@@ -28,6 +31,8 @@ class UserDefinedNMF:
                 error = r - np.dot(self.user_factors[u], self.item_factors[i])
                 self.user_factors[u] += self.learning_rate * (error * self.item_factors[i] - self.reg_pu * self.user_factors[u])
                 self.item_factors[i] += self.learning_rate * (error * self.user_factors[u] - self.reg_qi * self.item_factors[i])
+                
+
 
             if epoch % 10 == 0 and epoch > 0:
                 self.learning_rate *= 0.9
@@ -44,7 +49,7 @@ class UserDefinedNMF:
                 u_mapped = self.user_id_map.get(u, None) if self.user_id_map else u
                 i_mapped = self.item_id_map.get(i, None) if self.item_id_map else i
 
-                if u_mapped is None or i_mapped is None: #user_id_map and item_id_map not defined
+                if u_mapped is None or i_mapped is None:
                     print(f"Skipping prediction for uid: {u} and iid: {i} - ID out of bounds")
                     continue
 
